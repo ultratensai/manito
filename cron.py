28b7e2c8faf60e8634184f71a3c743646269933f
Jae Hak Kim <flatwhite@j1.nicewarm.coffee>
2020-09-12 17:13:48 +1000

    Hide tokens

---
+++ cron.py
@@ -0,0 +1,22 @@
+#!/home/flatwhite/botato/bin/python
+from twilio.rest import Client
+import configparser
+
+config = configparser.ConfigParser()
+config.read('/etc/auth.conf')
+
+account_sid = config['manito_token']['sid']
+auth_token = config['manito_token']['token']
+
+client = Client(account_sid, auth_token)
+
+
+def send_msg(number, sms_text):
+    message = client.messages.create(body=sms_text, from_='manito', to=number)
+    print(message.sid)
+
+target = ['+61403646240']
+
+for t in target:
+    send_msg(t, 'This is pretty good  ❤️')
+    #send_msg(t, '오늘 엄청 더운데 더위 조심하세요!!')
